9e341af9ea3369d35aafba7ca72e022062011a4d
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2022-11-04 15:08:42 +0800

    Add files via upload

---
+++ modules/accurate_battery/customize.sh
@@ -0,0 +1,35 @@
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=false
+LATESTARTSERVICE=true
+
+print_modname(){
+ui_print "
+ ********************************************************
+ 
+ - 模块: $MODNAME
+ - 模块ID: $MODID
+ - 作者: $MODAUTHOR
+ -      模块介绍↓
+ - 使用C语言编写，GCC编译器静态编译
+ - 精准电量，干掉虚假的UI电量
+ - 电量判断至小数点后一位且采用四舍五入变为整数
+ - 程序很简单，开源地址：https://github.com/chase535/accurate_battery
+
+ ********************************************************
+
+ - 注意！由于此版本设计为涓流充电结束后才会变为100%
+ - 所以99%-100%充电会极为缓慢！
+ - 如不想将涓流充电过程加入电量统计，请使用后缀带no_trickle的版本！
+
+ ********************************************************
+ "
+}
+
+on_install(){
+    cp -af $TMPDIR/accurate_battery $MODPATH/accurate_battery
+}
+
+set_permissions(){
+    set_perm_recursive  $MODPATH  0  0  0777  0777
+}
---
+++ modules/accurate_battery_no_trickle/common/service.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+nohup $MODDIR/accurate_battery_no_trickle &

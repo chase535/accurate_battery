f69c7d1f9f6da532b6e54096ac125b0c5e37cb83
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2022-11-04 16:59:38 +0800

    Update accurate_battery_no_trickle.c

--- accurate_battery_no_trickle.c
+++ accurate_battery_no_trickle.c
@@ -32,25 +32,25 @@ int main()
 {
     FILE *fm;
     char battery[4];
-    int power;
+    int power[6];
     while(1)
     {
         memset(battery, '\0', sizeof(battery));
+        memset(power, '0', sizeof(power));
         fm = fopen("/sys/class/power_supply/bms/capacity_raw", "rt");
         if(fm != NULL)
         {
-            fscanf(fm, "%d", &power);
-            power += 50;
-            if(power>9999)
-                sprintf(battery, "100");
-            else if(power>999)
-                snprintf(battery, 3, "%d", power);
-            else if(power>99)
-                snprintf(battery, 2, "%d", power);
+            fscanf(fm, "%d", power);
+            *power += 50;
+            if(*power > 9999)
+                *battery = 100;
+            else if(*power > 999)
+                snprintf(battery, 3, "%d", *power);
+            else if(*power > 99)
+                snprintf(battery, 2, "%d", *power);
             else
-                sprintf(battery, "0");
+                *battery = 0;
             set_value("/sys/class/power_supply/battery/capacity", battery);
-            set_value("/sys/class/power_supply/bms/capacity", battery);
             fclose(fm);
             fm = NULL;
         }

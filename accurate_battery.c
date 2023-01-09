ea65ddf5cedd243711d10801e64a6fab899ec022
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-09 21:00:50 +0800

    Update accurate_battery.c

--- accurate_battery.c
+++ accurate_battery.c
@@ -43,9 +43,9 @@ int main(int argc, char *argv[])
         printf("请传入真实电量文件路径，勿传入多余参数！\n");
         exit(20);
     }
-    while(1)
+    if(strcmp(argv[1], "/sys/class/power_supply/bms/real_capacity") == 0)
     {
-        if(strcmp(argv[1], "/sys/class/power_supply/bms/real_capacity") == 0)
+        while(1)
         {
             fm = fopen(argv[1], "rt");
             if(fm != NULL)
@@ -59,66 +59,76 @@ int main(int argc, char *argv[])
                 printf("无法读取%s文件，程序强制退出！\n", argv[1]);
                 exit(2);
             }
+            sleep(5);
         }
-        else if(strcmp(argv[1], "/sys/class/power_supply/bms/capacity_raw") == 0)
+    }
+    else if(strcmp(argv[1], "/sys/class/power_supply/bms/capacity_raw") == 0)
+    {
+        while(1)
         {
             fm = fopen(argv[1], "rt");
             if(fm != NULL)
             {
                 fgets(battery, 6, fm);
-                power = atoi(battery);
-                power += 50;
-                if(power > 9999)
+                fclose(fm);
+                fm = NULL;
+            }
+            else
+            {
+                printf("无法读取%s文件，程序强制退出！\n", argv[1]);
+                exit(2);
+            }
+            power = atoi(battery);
+            power += 50;
+            if(power > 9999)
+            {
+                if(! full)
                 {
-                    if(! full)
+                    fp = fopen("/sys/class/power_supply/bms/current_now", "rt");
+                    if(fp != NULL)
                     {
-                        fp = fopen("/sys/class/power_supply/bms/current_now", "rt");
-                        if(fp != NULL)
-                        {
-                            fgets(current_char, 20, fp);
-                            fclose(fp);
-                            fp = NULL;
-                            current = atoi(current_char);
-                            full = (current == 0)?1:0;
-                        }
-                        else
-                        {
-                            printf("无法读取/sys/class/power_supply/bms/current_now文件，程序强制退出！\n");
-                            exit(1);
-                        }
-                        (full)?snprintf(battery, 4, "100"):snprintf(battery, 3, "99");
+                        fgets(current_char, 20, fp);
+                        fclose(fp);
+                        fp = NULL;
                     }
                     else
                     {
-                        snprintf(battery, 4, "100");
+                        printf("无法读取/sys/class/power_supply/bms/current_now文件，程序强制退出！\n");
+                        exit(1);
                     }
+                    current = atoi(current_char);
+                    full = (current == 0)?1:0;
+                    (full)?snprintf(battery, 4, "100"):snprintf(battery, 3, "99");
                 }
                 else
                 {
-                    full = 0;
-                    if(power > 999)
-                        snprintf(battery, 3, "%d", power);
-                    else if(power > 99)
-                        snprintf(battery, 2, "%d", power);
-                    else
-                        snprintf(battery, 2, "0");
+                    snprintf(battery, 4, "100");
                 }
-                set_value("/sys/class/power_supply/battery/capacity", battery);
-                fclose(fm);
-                fm = NULL;
             }
             else
             {
-                printf("无法读取%s文件，程序强制退出！\n", argv[1]);
-                exit(2);
+                full = 0;
+                if(power > 999)
+                {
+                    snprintf(battery, 3, "%d", power);
+                }
+                else if(power > 99)
+                {
+                    snprintf(battery, 2, "%d", power);
+                }
+                else
+                {
+                    snprintf(battery, 2, "0");
+                }
             }
+            set_value("/sys/class/power_supply/battery/capacity", battery);
+            sleep(5);
         }
-        else
-        {
-            printf("不支持的真实电量文件路径：%s，程序强制退出！\n", argv[1]);
-            exit(6);
-        }
-        sleep(5);
+    }
+    else
+    {
+        printf("不支持的真实电量文件路径：%s，程序强制退出！\n", argv[1]);
+        exit(6);
     }
     return 0;
 }

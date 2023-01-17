109e5e73511395e02150b06101e90045d54868a3
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-17 23:39:54 +0800

    更新安装脚本

--- module/customize.sh
+++ module/customize.sh
@@ -18,12 +18,9 @@ echo ''
 
 if [[ -f /sys/class/power_supply/bms/real_capacity ]]; then
     echo ' - 检测到/sys/class/power_supply/bms/real_capacity文件存在'
-    echo ' - 将直接从此文件中读取电量，充到100%后仍会有充电电流！'
     echo 'capacity_file=/sys/class/power_supply/bms/real_capacity' >> $TMPDIR/service.sh
-    sed -i '/^description/d' $TMPDIR/service.sh
-    echo 'description=精准电量，干掉虚假的UI电量。注意！检测到/sys/class/power_supply/bms/real_capacity文件存在，将直接从此文件中读取电量，充到100%后仍会有充电电流！' >> $TMPDIR/service.sh
-    real_capacity="1"
 elif [[ -f /sys/class/power_supply/bms/capacity_raw ]]; then
+    echo ' - 检测到/sys/class/power_supply/bms/capacity_raw文件存在'
     echo 'capacity_file=/sys/class/power_supply/bms/capacity_raw' >> $TMPDIR/service.sh
 else
     echo '未找到真实电量文件，不支持此手机，模块刷入失败！'
@@ -31,24 +28,23 @@ else
 fi
 
 if [[ -f $TMPDIR/accurate_battery ]]; then
-    if [[ "$real_capacity" != "1" ]]; then
-        echo ' - 注意！由于此版本设计为涓流充电结束后才会变为100%'
-        echo ' - 所以99%-100%充电会极为缓慢！'
-        echo ' - 如不想将涓流充电过程加入电量统计，请使用后缀带no_trickle的版本！'
-    fi
+    echo ' - 注意！由于此版本设计为涓流充电结束后才会变为100%'
+    echo ' - 所以99%-100%充电会极为缓慢！'
+    echo ' - 如不想将涓流充电过程加入电量统计，请使用后缀带no_trickle的版本！'
     echo 'nohup $MODDIR/accurate_battery $capacity_file &' >> $TMPDIR/service.sh
-    cp -af $TMPDIR/accurate_battery $MODPATH/accurate_battery
+    cp -f $TMPDIR/accurate_battery $MODPATH/accurate_battery
+    chmod 777 $MODPATH/accurate_battery
 elif [[ -f $TMPDIR/accurate_battery_no_trickle ]]; then
-    if [[ "$real_capacity" != "1" ]]; then
-        echo ' - 注意！由于此版本设计为不将涓流充电过程加入电量统计'
-        echo ' - 所以充到100%后仍会有充电电流！'
-        echo ' - 如想将涓流充电过程加入电量统计，请使用后缀不带no_trickle的版本！'
-    fi
+    echo ' - 注意！由于此版本设计为不将涓流充电过程加入电量统计'
+    echo ' - 所以充到100%后仍会有充电电流！'
+    echo ' - 如想将涓流充电过程加入电量统计，请使用后缀不带no_trickle的版本！'
     echo 'nohup $MODDIR/accurate_battery_no_trickle $capacity_file &' >> $TMPDIR/service.sh
-    cp -af $TMPDIR/accurate_battery_no_trickle $MODPATH/accurate_battery_no_trickle
+    cp -f $TMPDIR/accurate_battery_no_trickle $MODPATH/accurate_battery_no_trickle
+    chmod 777 $MODPATH/accurate_battery_no_trickle
 else
     echo '缺少主程序，模块刷入失败！'
     exit 1
 fi
+
 echo ''
 echo ' ********************************************************'

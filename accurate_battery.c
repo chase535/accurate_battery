29d90db9391bc682fc7ee198655b63017e6ba42d
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-01-24 22:41:40 +0800

    合并两个版本

--- accurate_battery.c
+++ accurate_battery.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 {
     FILE *fm, *fp, *fq;
     char battery[10], current_char[30], charge_status[25];
-    int power, current, full=0;
+    int power, current, full=0,no_trickle=0;
     if(argc < 2)
     {
         printf("请传入真实电量文件路径！\n");
@@ -61,6 +61,7 @@ int main(int argc, char *argv[])
     {
         while(1)
         {
+            if(access("/data/adb/accurate_battery/no_trickle", F_OK) == 0) no_trickle=1;
             fm = fopen(argv[1], "rt");
             if(fm != NULL)
             {
@@ -73,51 +74,48 @@ int main(int argc, char *argv[])
                 printf("无法读取%s文件，程序强制退出！\n", argv[1]);
                 exit(2);
             }
-            fq = fopen("/sys/class/power_supply/battery/status", "rt");
-            if(fq != NULL)
+            if(!no_trickle)
             {
-                fgets(charge_status, 20, fq);
-                fclose(fq);
-                fq = NULL;
-            }
-            else
-            {
-                printf("无法读取/sys/class/power_supply/battery/status文件，程序强制退出！\n");
-                exit(10);
-            }
-            line_feed(charge_status);
-            power = atoi(battery);
-            if(power == 100)
-            {
-                if(strcmp(charge_status, "Charging") == 0 || strcmp(charge_status, "Full") == 0)
+                fq = fopen("/sys/class/power_supply/battery/status", "rt");
+                if(fq != NULL)
                 {
-                    if(!full)
-                    {
-                        fp = fopen("/sys/class/power_supply/battery/current_now", "rt");
-                        if(fp != NULL)
-                        {
-                            fgets(current_char, 20, fp);
-                            fclose(fp);
-                            fp = NULL;
-                        }
-                        else
-                        {
-                            printf("无法读取/sys/class/power_supply/battery/current_now文件，程序强制退出！\n");
-                            exit(30);
-                        }
-                        current = atoi(current_char);
-                        full=(current < 0)?0:1;
-                        (full)?:snprintf(battery, 3, "99");
-                    }
+                    fgets(charge_status, 20, fq);
+                    fclose(fq);
+                    fq = NULL;
                 }
                 else
                 {
-                    (full)?:snprintf(battery, 3, "99");
+                    printf("无法读取/sys/class/power_supply/battery/status文件，程序强制退出！\n");
+                    exit(10);
                 }
-            }
-            else
-            {
-                full = 0;
+                line_feed(charge_status);
+                power = atoi(battery);
+                if(power == 100)
+                {
+                    if(strcmp(charge_status, "Charging") == 0 || strcmp(charge_status, "Full") == 0)
+                    {
+                        if(!full)
+                        {
+                            fp = fopen("/sys/class/power_supply/battery/current_now", "rt");
+                            if(fp != NULL)
+                            {
+                                fgets(current_char, 20, fp);
+                                fclose(fp);
+                                fp = NULL;
+                            }
+                            else
+                            {
+                                printf("无法读取/sys/class/power_supply/battery/current_now文件，程序强制退出！\n");
+                                exit(30);
+                            }
+                            current = atoi(current_char);
+                            full=(current < 0)?0:1;
+                            (full)?:snprintf(battery, 3, "99");
+                        }
+                    }
+                    else (full)?:snprintf(battery, 3, "99");
+                }
+                else full = 0;
             }
             set_value("/sys/class/power_supply/battery/capacity", battery);
             sleep(5);
@@ -127,6 +125,7 @@ int main(int argc, char *argv[])
     {
         while(1)
         {
+            if(access("/data/adb/accurate_battery/no_trickle", F_OK) == 0) no_trickle=1;
             fm = fopen(argv[1], "rt");
             if(fm != NULL)
             {
@@ -139,67 +138,56 @@ int main(int argc, char *argv[])
                 printf("无法读取%s文件，程序强制退出！\n", argv[1]);
                 exit(2);
             }
-            fq = fopen("/sys/class/power_supply/battery/status", "rt");
-            if(fq != NULL)
-            {
-                fgets(charge_status, 20, fq);
-                fclose(fq);
-                fq = NULL;
-            }
-            else
-            {
-                printf("无法读取/sys/class/power_supply/battery/status文件，程序强制退出！\n");
-                exit(10);
-            }
             line_feed(charge_status);
             power = atoi(battery)+50;
             if(power > 9999)
             {
-                if(strcmp(charge_status, "Charging") == 0 || strcmp(charge_status, "Full") == 0)
+                if(no_trickle) snprintf(battery, 4, "100");
+                else
                 {
-                    if(full)
+                    fq = fopen("/sys/class/power_supply/battery/status", "rt");
+                    if(fq != NULL)
                     {
-                        snprintf(battery, 4, "100");
+                        fgets(charge_status, 20, fq);
+                        fclose(fq);
+                        fq = NULL;
                     }
                     else
                     {
-                        fp = fopen("/sys/class/power_supply/battery/current_now", "rt");
-                        if(fp != NULL)
-                        {
-                            fgets(current_char, 20, fp);
-                            fclose(fp);
-                            fp = NULL;
-                        }
+                        printf("无法读取/sys/class/power_supply/battery/status文件，程序强制退出！\n");
+                        exit(10);
+                    }
+                    if(strcmp(charge_status, "Charging") == 0 || strcmp(charge_status, "Full") == 0)
+                    {
+                        if(full) snprintf(battery, 4, "100");
                         else
                         {
-                            printf("无法读取/sys/class/power_supply/battery/current_now文件，程序强制退出！\n");
-                            exit(1);
+                            fp = fopen("/sys/class/power_supply/battery/current_now", "rt");
+                            if(fp != NULL)
+                            {
+                                fgets(current_char, 20, fp);
+                                fclose(fp);
+                                fp = NULL;
+                            }
+                            else
+                            {
+                                printf("无法读取/sys/class/power_supply/battery/current_now文件，程序强制退出！\n");
+                                exit(1);
+                            }
+                            current = atoi(current_char);
+                            full=(current < 0)?0:1;
+                            (full)?snprintf(battery, 4, "100"):snprintf(battery, 3, "99");
                         }
-                        current = atoi(current_char);
-                        full=(current < 0)?0:1;
-                        (full)?snprintf(battery, 4, "100"):snprintf(battery, 3, "99");
                     }
-                }
-                else
-                {
-                    (full)?snprintf(battery, 4, "100"):snprintf(battery, 3, "99");
+                    else (full)?snprintf(battery, 4, "100"):snprintf(battery, 3, "99");
                 }
             }
             else
             {
                 full = 0;
-                if(power > 999)
-                {
-                    snprintf(battery, 3, "%d", power);
-                }
-                else if(power > 99)
-                {
-                    snprintf(battery, 2, "%d", power);
-                }
-                else
-                {
-                    snprintf(battery, 2, "0");
-                }
+                if(power > 999) snprintf(battery, 3, "%d", power);
+                else if(power > 99) snprintf(battery, 2, "%d", power);
+                else snprintf(battery, 2, "0");
             }
             set_value("/sys/class/power_supply/battery/capacity", battery);
             sleep(5);

42381a4ab8b59152bc455cbd0eff130191ef3e22
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-09-01 13:00:09 +0800

    printf添加时间

--- accurate_battery.c
+++ accurate_battery.c
@@ -1,9 +1,74 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "time.h"
 #include "unistd.h"
 #include "sys/types.h"
 #include "sys/stat.h"
+
+#define printf_with_time(FORMAT,...)\
+{\
+    get_utc8_time(&time_utc8_now);\
+    printf("[ %04d.%02d.%02d %02d:%02d:%02d UTC+8 ] "FORMAT"\n", time_utc8_now.tm_year, time_utc8_now.tm_mon, time_utc8_now.tm_mday, time_utc8_now.tm_hour, time_utc8_now.tm_min, time_utc8_now.tm_sec, ##__VA_ARGS__);\
+    fflush(stdout);\
+}
+
+struct tm time_utc8_now;
+
+void get_utc8_time(struct tm *ptm)
+{
+    time_t cur_time;
+    time(&cur_time);
+    memcpy(ptm, gmtime(&cur_time), sizeof(struct tm));
+    ptm->tm_year+=1900;
+    ptm->tm_mon+=1;
+    ptm->tm_hour+=8;
+    if(ptm->tm_hour > 23)
+    {
+        ptm->tm_hour-=24;
+        ptm->tm_mday+=1;
+        switch(ptm->tm_mon)
+        {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                if(ptm->tm_mday > 31)
+                {
+                    ptm->tm_mday-=31;
+                    ptm->tm_mon+=1;
+                }
+                break;
+            case 2:
+                if(((ptm->tm_year%4 == 0) && (ptm->tm_year%100 != 0)) || (ptm->tm_year%400 == 0))
+                {
+                    if(ptm->tm_mday > 29)
+                    {
+                        ptm->tm_mday-=29;
+                        ptm->tm_mon+=1;
+                    }
+                }
+                else
+                {
+                    if(ptm->tm_mday > 28)
+                    {
+                        ptm->tm_mday-=28;
+                        ptm->tm_mon+=1;
+                    }
+                }
+                break;
+            default:
+                if(ptm->tm_mday > 30)
+                {
+                    ptm->tm_mday-=30;
+                    ptm->tm_mon+=1;
+                }
+                break;
+        }
+        if(ptm->tm_mon > 12)
+        {
+            ptm->tm_mon-=12;
+            ptm->tm_year+=1;
+        }
+    }
+}
 
 void set_value(char *file, char *numb)
 {
@@ -29,14 +94,14 @@ void set_value(char *file, char *numb)
             }
             else
             {
-                printf("无法向%s文件写入数据，程序强制退出！\n", file);
+                printf_with_time("无法向%s文件写入数据，程序强制退出！", file);
                 exit(3);
             }
         }
     }
     else
     {
-        printf("找不到%s文件，程序强制退出！\n", file);
+        printf_with_time("找不到%s文件，程序强制退出！", file);
         exit(4);
     }
 }
@@ -52,7 +117,7 @@ void check_file(char *file)
 {
     if(access(file, F_OK) != 0)
     {
-        printf("无法找到%s文件，程序强制退出！\n", file);
+        printf_with_time("无法找到%s文件，程序强制退出！", file);
         exit(999);
     }
 }
@@ -70,7 +135,7 @@ void read_file(char *file,char *value)
     }
     else
     {
-        printf("无法读取%s文件，程序强制退出！\n", file);
+        printf_with_time("无法读取%s文件，程序强制退出！", file);
         exit(30);
     }
 }
@@ -78,20 +143,20 @@ void read_file(char *file,char *value)
 int main(int argc, char *argv[])
 {
     char battery[10],current_char[30],charge_status[25];
-    int power,current,full=0,num=0,no_trickle,charge_full;
+    int power=0,current=0,full=0,num=0,no_trickle=100,charge_full=0;
     if(argc < 2)
     {
-        printf("请传入真实电量文件路径！\n");
+        printf_with_time("请传入真实电量文件路径！");
         exit(7);
     }
     else if(argc > 2)
     {
-        printf("请传入真实电量文件路径，勿传入多余参数！\n");
+        printf_with_time("请传入真实电量文件路径，勿传入多余参数！");
         exit(20);
     }
     else if(strcmp(argv[1], "/sys/class/power_supply/bms/real_capacity") != 0 && strcmp(argv[1], "/sys/class/power_supply/bms/capacity_raw") != 0)
     {
-        printf("不支持的真实电量文件路径：%s，程序强制退出！\n", argv[1]);
+        printf_with_time("不支持的真实电量文件路径：%s，程序强制退出！", argv[1]);
         exit(6);
     }
     check_file("/sys/class/power_supply/battery/status");
@@ -104,6 +169,16 @@ int main(int argc, char *argv[])
         read_file("/sys/class/power_supply/battery/current_now", current_char);
         current = atoi(current_char);
         read_file("/sys/class/power_supply/battery/status", charge_status);
+        if(access("/data/adb/accurate_battery/no_trickle", F_OK) == 0 && no_trickle == 0)
+        {
+            printf_with_time("/data/adb/accurate_battery/no_trickle文件存在，不将涓流充电过程加入电量统计");
+            no_trickle=1;
+        }
+        else if(access("/data/adb/accurate_battery/no_trickle", F_OK) != 0 && no_trickle == 1)
+        {
+            printf_with_time("/data/adb/accurate_battery/no_trickle文件不存在，将涓流充电过程加入电量统计");
+            no_trickle=0;
+        }
         no_trickle=(access("/data/adb/accurate_battery/no_trickle", F_OK) == 0)?1:0;
         if(strcmp(charge_status, "Full") == 0) num=(current <= 0)?num+1:0;
         else num=0;

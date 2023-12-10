a8df5b31234f5bf7b7f762ece310d1392a1abdb9
诺鸡鸭 <107602367+chase535@users.noreply.github.com>
2023-12-10 23:29:24 +0800

    开机时将上次运行时的日志文件重命名为log.txt.old

--- module/common/service.sh
+++ module/common/service.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 capacity_file=capacity_file_not_defined
+[[ -f /data/adb/accurate_battery/log.txt ]] && mv -f /data/adb/accurate_battery/log.txt /data/adb/accurate_battery/log.txt.old
 echo "等待手机启动完毕，以确保时间准确。若只看到这一行内容，请立即联系模块作者！" > /data/adb/accurate_battery/log.txt
 until [[ "$(getprop service.bootanim.exit)" == "1" ]]; do
     sleep 1
